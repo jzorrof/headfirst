f7ab7c5fc81427d4043324d29960531f8a5d7341
Joe Fan <jzorrof@gmail.com>
2015-03-12 16:23:14 +0800

    first commit

---
+++ Ex1_p13.py
@@ -0,0 +1,16 @@
+__author__ = 'joe_fan'
+
+cast = ["The Holy Grail", "The Life of Brian", "The Meaning of life"]
+
+# insert year
+cast.insert(1, 1975)
+cast.insert(3, 1979)
+cast.append(1983)
+print(cast)
+print(cast)
+# create new list
+cast_new = ["The Holy Grail", 1975,
+            "The Life of Brian", 1979,
+            "The Meaning of life", 1983]
+
+print(cast_new)
