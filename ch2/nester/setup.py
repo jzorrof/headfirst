61d39a2947b90517f20a56ab283fc52c1deb8f56
fanzhong <zhongfan@lewatek.com>
2015-03-13 10:46:59 +0800

    update

---
+++ ch2/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author = 'Joe',
+        author_email = 'jzorrof@gmail.com',
+        url = 'http://fanzhong.me',
+        description = 'A simple printer of nested lists',
+    )
