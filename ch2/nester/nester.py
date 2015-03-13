b19e3f457c434cefbd2e8a888095ff38df5d3351
fanzhong <zhongfan@lewatek.com>
2015-03-13 20:31:41 +0800

    add nester update

--- ch2/nester/nester.py
+++ ch2/nester/nester.py
@@ -1,6 +1,6 @@
 '''This is the standard way to include
 '''
-def print_lol(the_list):
+def print_lol(the_list, level=0):
     '''
     print nester list item
     '''
@@ -8,4 +8,6 @@ def print_lol(the_list):
         if isinstance(each_item, list):
             print_lol(each_item)
         else:
+            for tab_tag in range(level):
+                print('\t', end='')
             print(each_item)
