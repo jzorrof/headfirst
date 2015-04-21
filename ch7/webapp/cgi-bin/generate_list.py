2b902e4790d275cb1e0f052cc159e02828d102c0
jzorrof@gmail.com <jzorrof@gmail.com>
2015-04-21 14:28:36 +0800

    add cgi demo

--- ch7/webapp/cgi-bin/generate_list.py
+++ ch7/webapp/cgi-bin/generate_list.py
@@ -1,2 +1,16 @@
-#! /media/1T/project/headfirst/py3env/bin/python
-print('test')
+#! /Users/joe_fan/PycharmProjects/headfirst/py3env/bin/python
+import athletemodel
+import yate
+import glob
+
+data_file = glob.glob("data/*.txt")
+athletes = athletemodel.put_to_store(data_file)
+
+print(yate.start_response())
+print(yate.include_header('Coach Kelly'))
+print(yate.start_form('generate_timing_data.py'))
+print(yate.para('select'))
+for each_athletes in athletes:
+    print(yate.radio_button('which_athlete', athletes[each_athletes].name))
+print(yate.end_form('Select'))
+print(yate.include_footer({"Home":"/index.html"}))
--- ch7/webapp/cgi-bin/generate_timing_data.py
+++ ch7/webapp/cgi-bin/generate_timing_data.py
@@ -1,1 +1,19 @@
-#! /media/1T/project/headfirst/py3env/bin/python
+#! /Users/joe_fan/PycharmProjects/headfirst/py3env/bin/python
+
+import cgi
+import athletemodel
+import yate
+
+athletes = athletemodel.get_from_store()
+
+from_data = cgi.FieldStorage()
+athlete_name = from_data['which_athlete'].value
+
+print(yate.start_response())
+print(yate.include_header('Data'))
+print(yate.header("Athlete:" + athlete_name + " ,Dob" + athletes[athlete_name].dob + "."))
+print(yate.para("Top times:"))
+print(yate.u_list(athletes[athlete_name].top3))
+print(yate.include_footer({"Home":"/index.html", "selecter anthoer":"generate_list.py"}))
+
+
